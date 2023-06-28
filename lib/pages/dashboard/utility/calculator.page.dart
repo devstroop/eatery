e3893ba8249fde1e598eea8220e2914af183b1a1
pahariyatri <pahariyatri@gmail.com>
2023-06-29 03:01:24 +0530

    added basic layout for calculator

--- lib/pages/dashboard/utility/calculator.page.dart
+++ lib/pages/dashboard/utility/calculator.page.dart
@@ -1,22 +1,144 @@
-import 'package:eatery_db/eatery_db.dart';
+import 'package:eatery/references.dart';
 import 'package:flutter/material.dart';
 
-class CalculatorPage extends StatelessWidget {
-  const CalculatorPage({super.key});
+class CalculatorPage extends StatefulWidget {
+  const CalculatorPage({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  String _input = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text('Calculator'),
-          backgroundColor: Colors.transparent,
-          foregroundColor: Colors.white,
-          leading: IconButton(
-            icon: Icon(UIcons.regularStraight.arrow_left),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          )),
+        title: const Text('Calculator'),
+        backgroundColor: Color.fromARGB(0, 47, 24, 130),
+        foregroundColor: const Color.fromARGB(255, 0, 0, 0),
+        leading: IconButton(
+          icon: Icon(UIcons.regularStraight.arrow_left),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  _input,
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: GridView.count(
+                  crossAxisCount: 4,
+                  childAspectRatio: 1.5,
+                  children: [
+                    buildButton('AC',
+                        textColor: Colors.red,
+                        fontSize: 18,
+                        onPressed: clearInput),
+                    buildButton('=', textColor: Colors.orange, fontSize: 24),
+                    buildButton('+', textColor: Colors.orange, fontSize: 24),
+                    buildButton('DEL',
+                        textColor: Colors.grey,
+                        fontSize: 16,
+                        onPressed: deleteLastChar),
+                    buildButton('/', textColor: Colors.orange, fontSize: 24),
+                    buildButton('7',
+                        fontSize: 24, onPressed: () => addInput('7')),
+                    buildButton('8',
+                        fontSize: 24, onPressed: () => addInput('8')),
+                    buildButton('9',
+                        fontSize: 24, onPressed: () => addInput('9')),
+                    buildButton('x', textColor: Colors.orange, fontSize: 24),
+                    buildButton('4',
+                        fontSize: 24, onPressed: () => addInput('4')),
+                    buildButton('5',
+                        fontSize: 24, onPressed: () => addInput('5')),
+                    buildButton('6',
+                        fontSize: 24, onPressed: () => addInput('6')),
+                    buildButton('-', textColor: Colors.orange, fontSize: 24),
+                    buildButton('1',
+                        fontSize: 24, onPressed: () => addInput('1')),
+                    buildButton('2',
+                        fontSize: 24, onPressed: () => addInput('2')),
+                    buildButton('3',
+                        fontSize: 24, onPressed: () => addInput('3')),
+                    buildButton('', fontSize: 20),
+                    buildButton('0',
+                        fontSize: 24, onPressed: () => addInput('0')),
+                    buildButton('.',
+                        fontSize: 24, onPressed: () => addInput('.')),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget buildButton(String text,
+      {Color? textColor, double fontSize = 20, VoidCallback? onPressed}) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black.withOpacity(0.2),
+          width: 1,
+        ),
+      ),
+      child: TextButton(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+            const EdgeInsets.all(8),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color?>(null),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            color: textColor ?? Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void addInput(String value) {
+    setState(() {
+      _input += value;
+    });
+  }
+
+  void clearInput() {
+    setState(() {
+      _input = '';
+    });
+  }
+
+  void deleteLastChar() {
+    setState(() {
+      if (_input.isNotEmpty) {
+        _input = _input.substring(0, _input.length - 1);
+      }
+    });
   }
 }
