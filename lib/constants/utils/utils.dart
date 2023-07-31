964c3cca9651869fe705fec4ed8491321a4f25fb
Akash Shah <aksbju@gmail.com>
2023-07-31 10:05:31 -0700

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	renamed:    lib/constants/global_variables.dart -> lib/constants/common.dart
    	deleted:    lib/constants/utils/utils.dart
    	new file:   lib/pages/dashboard/diningTable/search_diningTable.delegate.dart
    	deleted:    lib/pages/dashboard/pos/views/dininigTableSelection.view.dart
    	new file:   lib/widgets/dialogs/showMessageDialog.dart
    
    Changes not staged for commit:
    	modified:   lib/components/bottomsheets/upload_image_bottomsheet.dart
    	modified:   lib/components/detailed_product_view.dart
    	modified:   lib/components/dining_table_card.dart
    	modified:   lib/components/pos_order_type_selection_button.dart
    	modified:   lib/components/product_card.dart
    	modified:   lib/constants/common.dart
    	modified:   lib/main.dart
    	modified:   lib/pages/authentication/login.page.dart
    	modified:   lib/pages/backupRestore/backupRestore.page.dart
    	modified:   lib/pages/createCompany/components/body6.dart
    	modified:   lib/pages/createCompany/createCompany.page.dart
    	modified:   lib/pages/dashboard/customer/addCustomer.page.dart
    	modified:   lib/pages/dashboard/customer/customers.page.dart
    	modified:   lib/pages/dashboard/customer/editCustomer.page.dart
    	modified:   lib/pages/dashboard/customer/search_customer.delegate.dart
    	modified:   lib/pages/dashboard/customer/view.customer.page.dart
    	modified:   lib/pages/dashboard/dashboard.page.dart
    	modified:   lib/pages/dashboard/diningTable/addDiningTable.page.dart
    	modified:   lib/pages/dashboard/diningTable/category/addDiningTableCategory.page.dart
    	modified:   lib/pages/dashboard/diningTable/category/editDiningTableCategory.page.dart
    	modified:   lib/pages/dashboard/diningTable/diningTables.page.dart
    	modified:   lib/pages/dashboard/diningTable/editDiningTable.page.dart
    	modified:   lib/pages/dashboard/diningTable/search_diningTable.delegate.dart
    	modified:   lib/pages/dashboard/importExport/importExport.page.dart
    	modified:   lib/pages/dashboard/pos/checkout.page.dart
    	modified:   lib/pages/dashboard/pos/orderConfirmation.page.dart
    	modified:   lib/pages/dashboard/pos/pointOfSale.page.dart
    	modified:   lib/pages/dashboard/pos/views/cart.view.dart
    	modified:   lib/pages/dashboard/pos/views/kProduct.view.dart
    	modified:   lib/pages/dashboard/product/inventoryItem/addInventoryItem.page.dart
    	modified:   lib/pages/dashboard/product/inventoryItem/editInventoryItem.page.dart
    	modified:   lib/pages/dashboard/product/inventoryItem/inventoryItems.page.dart
    	modified:   lib/pages/dashboard/product/kitchenDishes/addKitchenDish.page.dart
    	modified:   lib/pages/dashboard/product/kitchenDishes/editKitchenDish.page.dart
    	modified:   lib/pages/dashboard/product/kitchenDishes/kitchenDishes.page.dart
    	modified:   lib/pages/dashboard/product/productCategory/addProductCategory.page.dart
    	modified:   lib/pages/dashboard/product/productCategory/editProductCategory.page.dart
    	modified:   lib/pages/dashboard/settings/company/editCompany.page.dart
    	modified:   lib/pages/dashboard/settings/currency/showCurrencyRegion.page.dart
    	modified:   lib/pages/dashboard/settings/taxSlab/addTaxSlab.page.dart
    	modified:   lib/pages/dashboard/settings/taxSlab/editTaxSlab.page.dart
    	modified:   lib/pages/dashboard/staff/addStaff.page.dart
    	modified:   lib/pages/dashboard/staff/editStaff.page.dart
    	modified:   lib/pages/dashboard/transaction/transactionDetails.page.dart
    	modified:   lib/pages/dashboard/utility/imageLibrary.page.dart
    	modified:   lib/references.dart
    	modified:   lib/services/utility/file.utility.service.dart
    	modified:   lib/services/utility/library_image.dart
    	modified:   lib/widgets/dialogs/showConfirmationDialog.dart
    	modified:   lib/widgets/dialogs/showMessageDialog.dart
    	modified:   lib/widgets/posWidgets/circularCategory.posWidget.dart

--- lib/constants/utils/utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:eatery/references.dart';
-
-void showSnackBar(BuildContext context, String text) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text(text),
-    ),
-  );
-}
