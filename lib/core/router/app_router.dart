58d13dcb2f35c285c54dcc35d619ab5b29ccc6b3
Akash Shah <akash@devstroop.com>
2026-07-16 18:47:00 +0530

    Add missing eatery_db import to app_router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:eatery_core/data/database/eatery_database.dart';
+import 'package:eatery_core/data/models/eatery_db.dart';
 import 'package:eatery_core/providers/auth_session.dart';
 import 'package:eatery_core/providers/role_provider.dart';
 import 'package:eatery/pages/authentication/login.page.dart';
